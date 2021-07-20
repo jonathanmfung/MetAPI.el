f7c11d33a01db7a82a60a543542ba94e8c9e5186
jonathanmfung <jonathanfung2000@gmail.com>
2021-07-20 00:55:08 -0700

    fix Version syntax and add emacs version

--- MetAPI.el
+++ MetAPI.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2021  Jonathan Fung
 
 ;; Author: Jonathan Fung <jonathanfung2000@gmail.com>
-;; Version 0.1
-;; Package-Requires: ((dash "2.19.0"))
+;; Version: 0.1
+;; Package-Requires: ((emacs "28")(dash "2.19.0"))
 ;; Keywords: art, API
 ;; URL: https://github.com/jonathanmfung/MetAPI.el
 
